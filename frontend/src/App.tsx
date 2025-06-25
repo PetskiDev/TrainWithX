471167e17907a83dcc4fbc94474c9c18bb472a5b
Petar <petar.kovacovski@gmail.com>
2025-06-25 22:36:33 +0200

    [not important] warning unused

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import PlansPage from './pages/Plans/PlansPage';
 import LoginPage from '@frontend/pages/LoginPage';
